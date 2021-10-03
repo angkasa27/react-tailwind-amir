f61956ab897e28bb25ec390c5ee713e07cfdd4d7
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2021-10-03 09:14:40 +0700

    Update for gh-pages ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
       <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
-      <p className="text-primary mt-5 text-lg">
+      <p className="desc">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
         consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
         eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
